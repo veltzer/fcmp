9832bc8d22c829af182f6cbb3e6d6b7548ee98a6
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-05 02:16:02 +0200

    more cleaning

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -rf autom4te.cache config.cache config.status config.log Makefile aclocal.m4 missing configure config.sub config.guess install-sh Makefile.in depcomp missing mkinstalldirs ltmain.sh libtool config.h *.lo *.o *.la stamp-h1 .deps .pc fcmp.pc
+rm -rf autom4te.cache config.cache config.status config.log Makefile aclocal.m4 missing configure config.sub config.guess install-sh Makefile.in depcomp missing mkinstalldirs ltmain.sh libtool config.h *.lo *.o *.la stamp-h1 .deps .pc fcmp.pc .libs
