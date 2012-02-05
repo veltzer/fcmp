66a754be3efee13a76c25d4edfd774379a06f3f1
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-05 03:10:14 +0200

    more cleaning

--- clean.sh
+++ clean.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 rm -rf autom4te.cache config.cache config.status config.log Makefile aclocal.m4 missing configure config.sub config.guess install-sh Makefile.in depcomp missing mkinstalldirs ltmain.sh libtool config.h *.lo *.o *.la stamp-h1 .deps .pc fcmp.pc .libs
+rm -rf debian/tmp/ debian/fcmp-dev debian/fcmp1
