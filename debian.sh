184165867550ab6f97439b1473fa963852f1b667
Mark Veltzer <mark.veltzer@gmail.com>
2012-02-05 03:02:49 +0200

    modified sig

--- debian.sh
+++ debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git archive --format=tar --prefix=fcmp_1.2.2/ HEAD | gzip > ../fcmp_1.2.2.orig.tar.gz
-dh_make --packagename fcmp_1.2.2 --library --copyright gpl2 --email mark.veltzer@gmail.com
+dh_make --packagename fcmp_1.2.2 --library --copyright gpl2 --email mark@veltzer.net
 rm -f ../fcmp_1.2.2.orig.tar.gz
